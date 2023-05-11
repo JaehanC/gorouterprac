1e8ce006949322b2a8012f10c9c5b52b4858171f
simbaat-korea <dev@simbaat.com>
2023-05-11 09:30:58 +0900

    Feat: gorouter setting

---
+++ android/app/src/main/kotlin/com/example/routerpractice/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.routerpractice
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:routerpractice/layout/default_layout.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+      body: Container(),
+    );
+  }
+}
